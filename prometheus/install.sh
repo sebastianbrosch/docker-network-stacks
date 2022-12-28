0bf6ee6a35e5a2ef2c2e33a236414263a8c15e07
sebastianbrosch <code@sebastianbrosch.de>
2022-12-28 01:44:17 +0100

    added variables for ipv4 and replacement for install script

--- prometheus/install.sh
+++ prometheus/install.sh
@@ -1,3 +1,4 @@
-# create network and stack.
+sed -i "s/((IPV4))/$SERVER_IPV4/g" $(dirname $(realpath "$BASH_SOURCE"))/prometheus.yml
+sed -i "s/((IPV4))/$SERVER_IPV4/g" $(dirname $(realpath "$BASH_SOURCE"))/grafana.ini
 docker network inspect traefik-public >/dev/null 2>&1 || docker network create --driver=overlay traefik-public
 docker stack deploy -c $(dirname $(realpath "$BASH_SOURCE"))/docker-compose.yml prometheus
